45ccb8f3b843c4105e7bd907e236b8b211ec08c4
melaniec207 <carrillomelanie54@gmail.com>
2025-06-19 21:25:35 +0000

    added title and welcome with name

---
+++ script.js
@@ -0,0 +1,11 @@
+const txtBox = document.getElementById("name");
+
+let name = "";
+const header = document.getElementById("h");
+
+document.getElementById("click").addEventListener("click",()=>
+{
+    event.preventDefault();
+    name = txtBox.value;
+    header.textContent = "Welcome, " + name + "!";
+});
